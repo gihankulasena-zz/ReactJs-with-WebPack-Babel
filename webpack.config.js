99b3c5ff77837e9fa292e472444d04a5fbb9ca62
Gihan Kanishka Kulasena <g.kulasena@H2Compute-CO-LK.local>
2018-04-18 15:13:59 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: path.resolve('src/index.js'),
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            { 
+                test: /.js$/,
+                loader: 'babel-loader',
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    }
+
+};
+
+module.exports = config;
